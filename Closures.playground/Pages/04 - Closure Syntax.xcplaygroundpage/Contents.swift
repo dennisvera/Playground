f6bb0766263d3882dd963966d6db799af9f4ece1
Dennis Vera <dennis.vera@wework.com>
2020-02-25 22:41:59 -0500

    closure syntax exercises

--- Closures.playground/Pages/04 - Closure Syntax.xcplaygroundpage/Contents.swift
+++ Closures.playground/Pages/04 - Closure Syntax.xcplaygroundpage/Contents.swift
@@ -5,11 +5,30 @@
 typealias Operate = (Int, Int) -> Int
 // --------------------------------------
 
+let longClosure = { (a: Int, b: Int) -> Int in
+  a * b
+}
 
+let noParametersType: Operate = { (a, b) -> Int in
+  a * b
+}
 
+let noReturnType: Operate = { (a, b) in
+  a * b
+}
 
+let shortClosure: Operate = { $0 * $1 }
 
+longClosure(4, 2)
+noParametersType(4, 2)
+noReturnType(4, 2)
+shortClosure(4, 2)
 
+let voidClosure: () -> Void = {
+  print("This is a void closure")
+}
+
+voidClosure()
 
 // --------------------------------------
 func printResult(_ a: Int, _ b: Int, _ operate: Operate) {
@@ -17,6 +36,9 @@ func printResult(_ a: Int, _ b: Int, _ operate: Operate) {
 }
 // --------------------------------------
 
+printResult(10, 3, { ($0 * $1 + 10) })
 
+// Trailing Closure Syntax
+printResult(10, 3) { $0 * $1 + 10 }
 
 //: [⇒ Next: 05 - Challenge - Closure Syntaxes](@next)
