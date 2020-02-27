f23d4de4fb86643b922190e280cceb3e4566f395
Dennis Vera <dennis.vera@wework.com>
2020-02-26 22:25:25 -0500

    filter and map exercises

--- Closures.playground/Pages/06 - forEach & map.xcplaygroundpage/Contents.swift
+++ Closures.playground/Pages/06 - forEach & map.xcplaygroundpage/Contents.swift
@@ -1,5 +1,3 @@
-//: [⇐ Previous: 05 - Challenge - Closure Syntax](@previous)
-//: ## Episode 06: forEach & map
 import Foundation
 
 // --------------------------------------
@@ -8,13 +6,33 @@ var prices = [1.50, 10.00, 4.99, 2.30, 8.19]
 
 //: `forEach` - Loops over a collection and performs an operation
 
+//:  `for loop`
+for price in prices {
+  print(price)
+}
 
+//: `forEach`
+prices.forEach { print($0) }
 
 
 //: `map` - Loops over an array, executes closure code, returns a new array
 
+//:  `for loop`
+var forSalePricesArray: [Double] = []
+for price in prices {
+  forSalePricesArray.append(price * 0.9)
+}
+
+//: ` map`
+let salePrices = prices.map { $0 * 0.5 }
+let priceLabel = salePrices.map { (price) -> String in
+  String(format: "%.f", price)
+}
+
+let priceLabel_2 = salePrices.map { String(format: "%.f", $0) }
 
 
-
-
-//: [⇒ Next: 07 - compactMap & flatMap](@next)
+print(forSalePricesArray)
+print(salePrices)
+print(priceLabel)
+print(priceLabel_2)
