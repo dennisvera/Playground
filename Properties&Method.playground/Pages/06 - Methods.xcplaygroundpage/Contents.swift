6229e2d2cfdd1981045c6b48c19eb79c7e081d9e
Dennis Vera <dennis.vera@wework.com>
2020-03-03 16:56:53 -0500

    type methods exercises

--- Properties&Method.playground/Pages/06 - Methods.xcplaygroundpage/Contents.swift
+++ Properties&Method.playground/Pages/06 - Methods.xcplaygroundpage/Contents.swift
@@ -1,7 +1,53 @@
-//: [⇐ Previous: 05 - Challenge: Properties](@previous)
 //: ## Episode 06: Methods
 
+enum Weekday: CaseIterable {
+  case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+
+  mutating func advance(by dayCount: UInt) {
+    let indexOfToday = Weekday.allCases.firstIndex(of: self)!
+    let indexOfAdvancedDay = indexOfToday + Int(dayCount)
+    self = Weekday.allCases[indexOfAdvancedDay % Weekday.allCases.count]
+  }
+}
+
+Weekday.allCases
+var weekday: Weekday = .tuesday
+weekday.advance(by: 2)
 
 
+struct Time {
+  var day: Weekday
+  var hour: UInt
 
-//: [⇒ Next: 07 - Challenge - Methods](@next)
+  init(day: Weekday, hour: UInt = 0) {
+    self.day = day
+    self.hour = hour
+  }
+
+  mutating func advance(byHours hourCount: UInt) {
+    self = self.advanced(byHours: hourCount)
+  }
+
+  func advanced(byHours hourCount: UInt) -> Time {
+    let (dayCount, hour) = (self.hour + hourCount).quotientAndRemainder(dividingBy: 24)
+
+    var time = self
+    time.day.advance(by: dayCount)
+    time.hour = hour
+    return time
+  }
+}
+
+let time = Time(day: .tuesday)
+var advancedTime = time.advanced(byHours: 24 * 2 + 3)
+advancedTime.advance(byHours: 6)
+
+// Type methods
+enum Mathematics {
+  static func getLength(x: Double, y: Double) -> Double {
+    (x * x + y * y).squareRoot()
+  }
+}
+
+Mathematics.getLength(x: 3, y: 4)
+
