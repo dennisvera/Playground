1d8099331ed648724f27c95d780cec7ea4511cce
Dennis Vera <dennis.vera@wework.com>
2020-03-02 23:03:26 -0500

    associated values and enums exercises

--- Enumeration.playground/Pages/07 - Associated Values.xcplaygroundpage/Contents.swift
+++ Enumeration.playground/Pages/07 - Associated Values.xcplaygroundpage/Contents.swift
@@ -1,7 +1,40 @@
-//: [⇐ Previous: 06 - Challenge - Switch Statements](@previous)
 //: ## Episode 07: Enumerations with Associated Values
 
+enum TwoDimensionalPoints {
+  case origin
+  case onYAxis(Double)
+  case onXAxis(Double)
+  case noZeroCoordinate(x: Double, y: Double)
+}
 
+let coordinates = (0.0, 7.0)
+let twoDimensionalPoint: TwoDimensionalPoints
 
+switch coordinates {
+case (0, 0):
+  twoDimensionalPoint = .origin
+case (_, 0):
+  twoDimensionalPoint = .onXAxis(coordinates.0)
+case (0, _):
+  twoDimensionalPoint = .onYAxis(coordinates.1)
+default:
+  twoDimensionalPoint = .noZeroCoordinate(x: coordinates.0, y: coordinates.1)
+}
 
-//: [⇒ Next: 08 - Conclusion](@next)
+func getValues(for point: TwoDimensionalPoints) -> (x: Double, y: Double) {
+  switch point {
+  case .origin:
+    return (0, 0)
+  case let .onXAxis(x):
+    return (x, 0)
+  case let .onYAxis(y):
+    return (0, y)
+  case let .noZeroCoordinate(x: x, y: y):
+    return (x, y)
+  }
+}
+
+getValues(for: .origin)
+getValues(for: .onXAxis(66.6))
+getValues(for: .onYAxis(-3.5))
+getValues(for: .noZeroCoordinate(x: 4, y: 4))
