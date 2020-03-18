7a74d37a7f4f3e7c04c0df84a7273d8591c09915
Dennis Vera <dennis.vera@wework.com>
2020-03-17 23:32:33 -0400

    Class inheretence lesson

--- Protocols&Inheritance.playground/Pages/02 - Inheritance.xcplaygroundpage/Contents.swift
+++ Protocols&Inheritance.playground/Pages/02 - Inheritance.xcplaygroundpage/Contents.swift
@@ -1,5 +1,3 @@
-//: [⇐ Previous: 01 - Introduction](@previous)
-//: ## Episode 02: Inheritance
 
 struct Grade {
   var letter: Character
@@ -17,20 +15,54 @@ class Person {
   }
 }
 
-class Student {
+class Student: Person {
   var grades: [Grade] = []
-  
-  var firstName: String
-  var lastName: String
-  
-  init(firstName: String, lastName: String) {
-    self.firstName = firstName
-    self.lastName = lastName
+
+}
+
+class SchoolBandMember: Student {
+  var minimumPracticeTime = 2
+}
+
+class StudentAthlete: Student {
+  override var grades: [Grade] {
+    didSet {
+      if !isEligible {
+        print("It's time to study!")
+      }
+    }
+  }
+  var isEligible: Bool {
+    return grades.filter { $0.letter == "F" }.count < 3
   }
 }
 
+let jon = Person(firstName: "Jon", lastName: "Snow")
+let jane = Student(firstName: "jane", lastName: "Snane")
+let jessy = SchoolBandMember(firstName: "Jessy", lastName: "Catterwul")
+let marty = StudentAthlete(firstName: "Marty", lastName: "Pants")
 
+let array = [jane, jessy, marty]
 
+let student = marty as Student
+let athlete = student as! StudentAthlete
 
+let utterFailureGrade = Grade(letter: "F", points: 0, credits: 0)
+athlete.grades.append(utterFailureGrade)
+athlete.grades.append(utterFailureGrade)
+athlete.grades.append(utterFailureGrade)
 
-//: [⇒ Next: 03 - Computed Properties](@next)
+func getEveningActivity(student: Student) -> String {
+  if let bandMember = student as? SchoolBandMember {
+    return "Practicing for at least \(bandMember.minimumPracticeTime)"
+  } else {
+    return "Hitting the books!"
+  }
+}
+
+getEveningActivity(student: jessy)
+getEveningActivity(student: jane)
+
+let historyGrade = Grade(letter: "B", points: 9, credits: 3)
+jane.grades.append(historyGrade)
+
