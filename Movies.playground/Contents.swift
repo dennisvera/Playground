70210614552af932955e0aff52c9938975341596
DennisVera <dennisvera.z@gmail.com>
2020-06-01 12:16:23 -0400

    Create API Client helper methods

--- Movies.playground/Contents.swift
+++ Movies.playground/Contents.swift
@@ -1,33 +1,10 @@
 import Foundation
 
-// MARK: - Types
-
-enum MovieApi: String {
-  case apiKey = "2631ea46e7edd7894cf3eaee7d263667"
-  case language = "en-US"
-  case page = "1"
-}
-
-// Define URL
-
-var components = URLComponents()
-components.scheme = "https"
-components.host = "api.themoviedb.org"
-components.path = "/3/movie/now_playing"
-components.queryItems = [
-  URLQueryItem(name: "api_key", value: MovieApi.apiKey.rawValue),
-  URLQueryItem(name: "language", value: MovieApi.language.rawValue),
-  URLQueryItem(name: "page", value: MovieApi.page.rawValue)
-]
-
-let baseUrl = components.url!
-
-APIClient(baseUrl: baseUrl).fetchMovies { result in
-  print(baseUrl)
+// Fetch Movies
+APIClient().fetchMovies { result in
   switch result {
   case .success(let movies):
     print(movies.results.count)
-    // print first movie title
     print(movies.results[0].title)
   case .failure(let error):
     print(error)
--- Movies.playground/Sources/APIClient.swift
+++ Movies.playground/Sources/APIClient.swift
@@ -1,54 +1,57 @@
 import Foundation
 
 final public class APIClient {
-
+  
   // MARK: - Types
-
+  
   public enum APIClientError: Error {
     case requestFailed
     case invalidResponse
   }
-
-  // MARK: - Properties
-
-  private let baseUrl: URL
-
-  // MARK: - Initializer
-
-  public init(baseUrl: URL) {
-    self.baseUrl = baseUrl
-  }
-
-  // MARK: - Public API
-
-  public func fetchMovies(_ completion: @escaping (Result<Movie, APIClientError>) -> Void) {
-    let url = baseUrl
     
-    // Create Request
-    let request = URLRequest(url: url)
-
+  private enum Components: String {
+    case apiKey = "2631ea46e7edd7894cf3eaee7d263667"
+    case language = "en-US"
+    case page = "1"
+  }
+  
+  private enum Endpoint: String {
+    case now_playing
+    
+    var path: String {
+      return rawValue
+    }
+  }
+  
+  // MARK: - Initializer
+  
+  public init() { }
+  
+  // MARK: - Public API
+  
+  public func fetchMovies(_ completion: @escaping (Result<Movie, APIClientError>) -> Void) {
     // Create and Initiate Data Task
-    URLSession.shared.dataTask(with: request) { (data, response, error) in
+    URLSession.shared.dataTask(with: request(for: .now_playing)) { (data, response, error) in
       if let data = data {
         do {
           // Initialize JSON Decoder
           let decoder = JSONDecoder()
-
+          
           // Configure JSON Decoder
           decoder.dateDecodingStrategy = .iso8601
           decoder.keyDecodingStrategy = .convertFromSnakeCase
-
+          
           // Decode JSON Response
           let movies = try decoder.decode(Movie.self, from: data)
-
+          
           completion(.success(movies))
-
+          
         } catch {
           completion(.failure(.invalidResponse))
         }
       } else {
         completion(.failure(.requestFailed))
-
+        
         if let error = error {
           print("Unable to fetch Movies: \(error)")
         } else {
@@ -56,5 +59,27 @@ final public class APIClient {
         }
       }
     }.resume()
+  }
+  
+  // MARK: - Helper Methods
+  
+  private func request(for endpoint: Endpoint) -> URLRequest {
+    // Create and Configure URL
+    var components = URLComponents()
+    components.scheme = "https"
+    components.host = "api.themoviedb.org"
+    components.path = "/3/movie/\(endpoint.path)"
+    components.queryItems = [
+      URLQueryItem(name: "api_key", value: Components.apiKey.rawValue),
+      URLQueryItem(name: "language", value: Components.language.rawValue),
+      URLQueryItem(name: "page", value: Components.page.rawValue)
+    ]
+    
+    guard let url = components.url else { fatalError("Unable to compose URL.") }
+    
+    // Create Request
+    let request = URLRequest(url: url)
+    
+    return request
   }
 }
